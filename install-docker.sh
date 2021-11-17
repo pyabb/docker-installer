8600b776cca735ddb4e0c6d0274b86e954cbcf33
raulqb <raulqb.89@gmail.com>
2021-11-16 20:30:05 -0500

    added docker composer

--- install-docker.sh
+++ install-docker.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/bash
+
+# Docker
 sudo apt-get remove docker docker-engine docker.io containerd runc
 sudo apt-get update -y
 sudo apt-get install ca-certificates curl gnupg lsb-release -y
@@ -8,3 +10,13 @@ echo \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+
+# Docker compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+# verify versions
+clear
+docker --version
+docker-compose --version
